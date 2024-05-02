ef423919512162a21276e2c22aaa7e09739e78d0
Romashka <GrebenevRV@gmail.com>
2024-05-03 00:11:36 +0300

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div>
+      <div className="container">
+        Контейнер
+      </div>
+    </div>
+  );
+}
